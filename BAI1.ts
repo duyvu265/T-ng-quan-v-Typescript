ca2e35003f843715b5592418d0f048a1a5909073
thanh duy <you@example.com>
2023-02-13 10:47:33 +0700

    BAI1

--- BAI1.ts
+++ BAI1.ts
@@ -1,2 +1,14 @@
 let x:number=0;
 let y:number=1;
+let sum:number=0;
+let sum1:number=0;
+for (let i = 0; i <20 ; i++) {
+    sum=x+y;
+    x=y;
+    y=sum;
+}
+console.log( sum1+=sum);
+
+
+
+
--- BAI2.ts
+++ BAI2.ts
@@ -3,7 +3,6 @@ let arr2: number[] = [0, 2, 3, 5, 7, 8, 9, 10];
 let arr3: number[] = [];
 let i: number = 0;
 while (i <= 10) {
-    arr1.indexOf(i);
     if ( arr2.indexOf(i)== -1) {
         arr3.push(i);
     }
